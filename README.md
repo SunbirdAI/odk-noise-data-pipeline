a7644dbb92c9a6b9c91cfb19afb72d82917c8b39
NLSanyu <lydiansanyu@gmail.com>
2021-06-15 09:55:58 +0300

    Set up pipeline

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# odk_noise_data_pipeline
+# ODK Noise Data Pipeline
 A pipeline for downloading, cleaning and transforming noise data from our ODK Central server
+
+### Overview
+This project uses the [ODK Central API](https://odkcentral.docs.apiary.io/#introduction/api-overview) to download the data. 
+
+# Running the project
+- Clone the repository: `git clone https://github.com/SunbirdAI/odk_noise_data_pipeline.git`
+- Create a `.env` file in the project root folder and fill in the env variables as shown in the `env.example` file
+- Run `python bulk_odk_download.py` to download the noise data from ODK
---
+++ bulk_odk_download.py
@@ -0,0 +1,68 @@
+import requests
+import logging
+from decouple import config
+from pathlib import Path
+
+logging.basicConfig(filename='app.log', 
+    format='%(asctime)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+base_url = config('ODK_PROJECT_URL')
+headers = {}
+service_params = {
+    'projectId': config('ODK_PROJECT_ID'),
+    'xmlFormId': config('ODK_FORM_ID')
+}
+
+def authenticate():
+    email = config('ODK_EMAIL')
+    password = config('ODK_PASSWORD')
+    login_url = f'{base_url}v1/sessions'
+
+    try:
+        response = requests.post(
+            login_url,
+            json={'email': email, 'password': password}
+        )
+    except requests.exceptions.RequestException as err:
+        logger.error(f'Authentication error: {err}')
+
+    if response.status_code != 200:
+        logger.error(f'Authentication failed')
+        return None
+
+    logger.info('Authenticated')
+    return response.json()['token']
+
+def get_submissions():
+    token = authenticate()
+    headers = {'Authorization': f'Bearer {token}'}
+
+    service_doc_url = f'{base_url}v1/projects/{service_params["projectId"]}/forms/{service_params["xmlFormId"]}.svc'
+
+    try:
+        service_doc_response = requests.get(service_doc_url, headers=headers)
+        if service_doc_response.status_code != 200:
+            logger.error(f'Service document error: {service_doc_response.text}')
+        logger.info('Service document response fetched')
+    except requests.exceptions.RequestException as err:
+        logger.error(f'Service document error: {err}')
+
+
+    # get ODK form submissions csv
+    csv_url = f'{base_url}v1/projects/{service_params["projectId"]}/forms/{service_params["xmlFormId"]}/submissions.csv.zip'
+    logger.info(csv_url)
+
+    try:
+        csv_response = requests.get(csv_url, headers=headers)
+        if csv_response.status_code != 200:
+            logger.error(f'Error fetching csv: {csv_response.text}')
+        logger.info('Csv/media response fetched')
+    except requests.exceptions.RequestException as err:
+        logger.error(f'Error fetching csv: {err}')
+
+if __name__=='__main__':
+    get_submissions()
